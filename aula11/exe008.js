e97ff3924a8c8be0453a5903e9a418084e730118
Dara Maria <darasousa09@gmail.com>
2020-11-24 17:18:37 -0300

    aula11

---
+++ aula11/exe008.js
@@ -0,0 +1,8 @@
+var velocidade = 75.4
+
+console.log(`A velocidade é ${velocidade} km/h.`)
+if(velocidade > 60){
+    console.log('Voce execedeu o limite de velocidade. MULTADO!')
+}
+
+console.log('Use sempre o cinto de segurança')
---
+++ aula11/exe009.js
@@ -0,0 +1,8 @@
+var pais = 'França'
+
+console.log(`Vivendo em ${pais}`)
+if(pais == 'brasil'){
+    console.log('Você é brasileiro!')
+}else{
+    console.log('Você é estrangeiro!')
+}
