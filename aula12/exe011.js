a23035b738de4db98a5330a8fa7522e30696f507
Dara Maria <darasousa09@gmail.com>
2020-11-27 14:48:37 -0300

    exe011

--- aula12/exe011.js
+++ aula12/exe011.js
@@ -1,1 +1,11 @@
-console.log('hello word')
+var idade = 75
+console.log(`Você tem ${idade} anos.`)
+
+if (idade < 16){
+    console.log('Não vota!')
+
+}else if (idade < 18 || idade > 65){
+    console.log('Voto opcional!')
+}else{
+    console.log('Voto obrigatorio!')
+}
