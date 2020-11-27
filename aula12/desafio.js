c40ce2a5da670aa6df81f073caf0de096d61e4c5
Dara Maria <darasousa09@gmail.com>
2020-11-27 16:03:16 -0300

    desafio

---
+++ aula12/desafio.js
@@ -0,0 +1,51 @@
+/*Mostra a data atual do sistema e a hora */
+var atual = new Date()
+var hora = atual.getHours() + ':' + atual.getMinutes() + ':' + atual.getSeconds()
+var mes = atual.getMonth()
+
+switch(mes){
+    case 0:
+        mes = 'Janeiro'
+        break
+    case 1:
+        mes = 'Feveireiro'
+        break
+    case 2:
+        mes = 'Março'
+        break
+    case 3:
+        mes = 'Abril'
+        break
+    case 4:
+        mes = 'Maio'
+        break
+    case 5:
+        mes = 'Junho'
+        break
+    case 6:
+        mes = 'Julho'
+        break
+    case 7:
+        mes = 'Agosto'
+        break
+    case 8:
+        mes = 'Setembro'
+        break
+    case 9:
+        mes = 'Outubro'
+        break
+    case 10:
+        mes = 'Novembro'
+        break
+    case 11:
+        mes = 'Dezembro'
+        break
+    default:
+        mes = 'Invalido'
+        break
+}
+
+var data = mes + ' de ' + atual.getFullYear()
+
+console.log(`Horario atual do sistema - ${hora}`)
+console.log(`Data atual do sistema - ${data}`)
